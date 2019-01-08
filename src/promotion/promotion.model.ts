8314643e2ffb63a405d5a3040fda49bca5c68570
ivan <ibodnar@outlook.com>
2019-01-08 19:19:54 -0300

    Logic for FamilyPromotion implemented - IPromotionResult interface added - checkPromotionApplies and applyPromotion fields added to interface IPromotion - enum AvailablePromotions added

--- src/promotion/promotion.model.ts
+++ src/promotion/promotion.model.ts
@@ -1,22 +1,58 @@
 import {Rental} from "../rental/rental.model";
 
 
-export interface IPromotion {
-    rentals: Rental[];
-    addRental: ( rental: Rental ) => void;
-    checkPromotionApplies: () => boolean;
+export enum AvailablePromotions {
+    familyPromotion = 'familyPromotion'
 }
 
+export interface IPromotion {
+    checkPromotionApplies: ( rentals: Rental[] ) => boolean;
+    applyPromotion: ( rentals: Rental[] ) => IPromotionResult;
+}
+
+export interface IPromotionResult {
+    promotion: AvailablePromotions;
+    promotedRentalsCost: number;
+    appliedPromotionRentals: Rental[];
+}
 
 export class FamilyPromotion implements IPromotion {
-    public rentals: Rental[] = [];
+    lowerLimit = 3;
+    upperLimit = 5;
+    upperIndex = this.upperLimit - 1;
+    discountPercentage = 30;
 
-    addRental( rental: Rental ): void {
-        this.rentals.push( rental );
+     _markRentalsForPromotion( rentals: Rental[] ): Rental[] {
+        rentals.forEach( (rental, index) => {
+            if (index <= this.upperIndex) {
+                rental.includedInPromotion = AvailablePromotions.familyPromotion;
+            }
+        });
+        return rentals;
     }
 
-    checkPromotionApplies(): boolean {
-        return this.rentals.length >= 3;
+    checkPromotionApplies( rentals: Rental[] ): boolean {
+        return rentals.length >= this.lowerLimit;
+    }
+
+    applyPromotion( rentals: Rental[] ): IPromotionResult {
+        const markedRentals = this._markRentalsForPromotion( rentals );
+        const promotedRentals = markedRentals.filter( rental => {
+            return rental.includedInPromotion === AvailablePromotions.familyPromotion
+        });
+
+        const promotedRentalsCost = promotedRentals.reduce( (acc, curr) => {
+                return acc + curr.calculateTotalCost()
+            }, 0
+        );
+
+        const costWithDiscount = promotedRentalsCost - ( ( promotedRentalsCost / 100 ) * this.discountPercentage );
+
+        return {
+            promotion: AvailablePromotions.familyPromotion,
+            promotedRentalsCost: costWithDiscount,
+            appliedPromotionRentals: markedRentals
+        } as IPromotionResult;
     }
     
 }
