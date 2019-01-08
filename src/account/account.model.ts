1d8ad86ece7db300b1ed16114af43a06aea9e426
ivan <ibodnar@outlook.com>
2019-01-08 19:17:37 -0300

    calculateCost added to Account

--- src/account/account.model.ts
+++ src/account/account.model.ts
@@ -1,11 +1,20 @@
 import {Rental} from "../rental/rental.model";
+import {CostBuilder} from "../cost-builder/cost-builder.model";
+import {IPromotion} from "../promotion/promotion.model";
+
 
 export class Account {
     constructor(
-        public rentals: Rental[] = []
+        public rentals: Rental[] = [],
+        public promotion: IPromotion
     ) { }
 
     addRental( rental: Rental ): void {
         this.rentals.push( rental );
+    }
+
+    calculateCost(): number {
+        const costBuilder = new CostBuilder( this.rentals, this.promotion );
+        return costBuilder.calculateCost();
     }
 }
