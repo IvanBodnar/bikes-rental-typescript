db3e7729b7e12c55535d4a01787eaf7f57b1156c
ivan <ibodnar@outlook.com>
2019-01-08 19:24:23 -0300

    Constructor structure changed on rental classes

--- src/rental/rental.model.ts
+++ src/rental/rental.model.ts
@@ -1,38 +1,51 @@
+import {AvailablePromotions} from "../promotion/promotion.model";
 
 export class Rental {
     price: number = 0;
     constructor(
-        public id: number
+        public id: number,
+        public timeConsumed: number = 0,
+        public includedInPromotion?: AvailablePromotions
     ) { }
 
-    calculateTotalCost( timeConsumed: number ): number {
-        return this.price * timeConsumed;
+    setTimeConsumed( time: number ): void {
+        this.timeConsumed = time;
+    }
+
+    calculateTotalCost(): number {
+        return this.price * this.timeConsumed;
     }
 }
 
 export class HourRental extends Rental {
     price: number = 5;
     constructor(
-        id: number,
+        public id: number,
+        public timeConsumed: number = 0,
+        public includedInPromotion?: AvailablePromotions
     ) {
-        super(id)
+        super(id, timeConsumed, includedInPromotion)
     }
 }
 
 export class DayRental extends Rental {
     price: number = 20;
     constructor(
-        id: number
+        public id: number,
+        public timeConsumed: number = 0,
+        public includedInPromotion?: AvailablePromotions
     ) {
-        super(id)
+        super(id, timeConsumed, includedInPromotion)
     }
 }
 
 export class WeekRental extends Rental {
-    price: number = 60;
     constructor(
-        id: number
+        public id: number,
+        public timeConsumed: number = 0,
+        public includedInPromotion?: AvailablePromotions
     ) {
-        super(id)
+        super(id, timeConsumed, includedInPromotion)
     }
+    price: number = 60;
 }
