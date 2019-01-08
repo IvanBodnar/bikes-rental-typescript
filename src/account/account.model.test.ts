d1cffac9be274765c7b8443545170ab084fe6fb5
ivan <ibodnar@outlook.com>
2019-01-08 19:17:54 -0300

    Account test added

--- src/account/account.model.test.ts
+++ src/account/account.model.test.ts
@@ -1,9 +1,15 @@
 import {Account} from "./account.model";
-import {HourRental} from "../rental/rental.model";
+import {DayRental, HourRental, WeekRental} from "../rental/rental.model";
+import {FamilyPromotion} from "../promotion/promotion.model";
 
 
 describe('Account tests', () => {
-    const account = new Account();
+    const rentals = [
+        new DayRental(1, 1),
+        new HourRental(2, 1),
+        new WeekRental(3, 1)
+    ];
+    const account = new Account( rentals, new FamilyPromotion() );
 
     test('Account creates', () => {
         expect( account ).toBeTruthy();
@@ -11,9 +17,13 @@ describe('Account tests', () => {
 
     test('Account.addRental', () => {
         account.addRental( new HourRental(1) );
-        expect( account.rentals.length ).toEqual(1 );
-        expect( account.rentals[0] ).toBeInstanceOf( HourRental );
-    })
+        expect( account.rentals.length ).toEqual(4 );
+        expect( account.rentals[0] ).toBeInstanceOf( DayRental );
+    });
+
+    test('calculateCost', () => {
+        expect( account.calculateCost() ).toEqual( 59.5 );
+    });
 });
 
 
